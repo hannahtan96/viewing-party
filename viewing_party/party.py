13090edebb5786e8ff73c1ed0f25981412cd1cbd
Hannah <hannahtan96@gmail.com>
2022-09-15 23:15:01 -0700

    wave2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -54,14 +54,33 @@ def watch_movie(user_data, title):
 
     return user_data
 
-
-
-
-
-
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    sum_of_ratings = 0
+    if user_data["watched"]:
+        for i in range(len(user_data["watched"])):
+            sum_of_ratings += user_data["watched"][i]["rating"]
+    else: return 0
+
+    average_of_ratings = sum_of_ratings / len(user_data["watched"])
+    return average_of_ratings
+
+
+def get_most_watched_genre(user_data):
+    genre_dict = {}
+    if user_data["watched"]:
+        for i in range(len(user_data["watched"])):
+            if user_data["watched"][i]["genre"] in genre_dict.keys():
+                genre_dict[user_data["watched"][i]["genre"]] += 1
+            else: genre_dict[user_data["watched"][i]["genre"]] = 1
+    else: return None
+
+    most_watched_genre = max(genre_dict.values())
+    for key, value in genre_dict.items():
+        if value == most_watched_genre:
+            return key
 
 
 # -----------------------------------------
